e385815922897fca10c87142cddcaaefce7e4d69
Clemens Wolff <clewolff@microsoft.com>
2017-04-27 12:10:52 -0700

    Formatting fix

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sources/instagram/dto/Domain.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sources/instagram/dto/Domain.scala
@@ -1,4 +1,16 @@
 package com.microsoft.partnercatalyst.fortis.spark.sources.instagram.dto
 
-case class Image(url: String, width: Int, height: Int)
-case class Instagram(thumbnail: Image, image: Image, id: String, link: String, createdAtEpoch: Long, imageUrl: String)
+case class Image(
+  url: String,
+  width: Int,
+  height: Int
+)
+
+case class Instagram(
+  thumbnail: Image,
+  image: Image,
+  id: String,
+  link: String,
+  createdAtEpoch: Long,
+  imageUrl: String
+)
--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sources/instagram/dto/Json.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sources/instagram/dto/Json.scala
@@ -1,6 +1,24 @@
 package com.microsoft.partnercatalyst.fortis.spark.sources.instagram.dto
 
-case class JsonInstagramResponse(data: List[JsonInstagramItem])
-case class JsonInstagramItem(created_time: String, `type`: String, images: JsonInstagramImages, id: String, link: String)
-case class JsonInstagramImages(standard_resolution: JsonInstagramImage, thumbnail: JsonInstagramImage)
-case class JsonInstagramImage(url: String, width: Int, height: Int)
+case class JsonInstagramResponse(
+  data: List[JsonInstagramItem]
+)
+
+case class JsonInstagramItem(
+  created_time: String,
+  `type`: String,
+  images: JsonInstagramImages,
+  id: String,
+  link: String
+)
+
+case class JsonInstagramImages(
+  standard_resolution: JsonInstagramImage,
+  thumbnail: JsonInstagramImage
+)
+
+case class JsonInstagramImage(
+  url: String,
+  width: Int,
+  height: Int
+)
