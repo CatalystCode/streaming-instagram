7f1b99ea7b7b48fe21b4eaf99f035de55b7eb93c
Clemens Wolff <clewolff@microsoft.com>
2017-05-03 10:00:00 -0700

    Improve reliability of flaky test

--- src/test/scala/com/github/catalystcode/fortis/spark/streaming/PollingFunctionSpec.scala
+++ src/test/scala/com/github/catalystcode/fortis/spark/streaming/PollingFunctionSpec.scala
@@ -8,12 +8,12 @@ import org.scalatest.FlatSpec
 class PollingFunctionSpec extends FlatSpec {
   "The polling function" should "execute a callback on a schedule" in {
     val executionCount = new AtomicInteger()
-    val poller = new PollingFunction(executionCount.incrementAndGet, PollingSchedule(7, TimeUnit.MILLISECONDS), 1)
+    val poller = new PollingFunction(executionCount.incrementAndGet, PollingSchedule(7, TimeUnit.MILLISECONDS, 8), 1)
 
     poller.start()
     Thread.sleep(11)
     poller.stop()
 
-    assert(executionCount.get == 2)
+    assert(executionCount.get == 1)
   }
 }
